49f9e1d6fbe6077fe40b74710a1a8b866e9caee5
Fabrice Brito <fabrice.brito@terradue.com>
2022-10-13 12:17:59 +0000

    test for CWL documents w/out a directory result

---
+++ src/tests/test_no_dir_output.py
@@ -0,0 +1,34 @@
+import os
+import tempfile
+import unittest
+
+from click.testing import CliRunner
+from cwl_wrapper import app
+
+
+class TestNoDirOutputCwl(unittest.TestCase):
+    def setUp(self):
+        self.stagein_cwl_file = os.path.join(os.path.dirname(__file__), "data/stagein.cwl")
+        self.stageout_cwl_file = os.path.join(os.path.dirname(__file__), "data/stageout.cwl")
+        self.app_cwl_file = os.path.join(os.path.dirname(__file__), "data/no_dir_output.cwl")
+        self.expected_wrapped_cwl = os.path.join(
+            os.path.dirname(__file__), "data/test_no_dir_output_expected.cwl"
+        )
+        self.expected_usage_test = os.path.join(os.path.dirname(__file__), "data/test_help_expected.txt")
+        self.temp_output_file = tempfile.NamedTemporaryFile()
+
+    def test_no_dir_output(self):
+        runner = CliRunner()
+        result = runner.invoke(
+            app.main,
+            [
+                "--stagein",
+                self.stagein_cwl_file,
+                "--stageout",
+                self.stageout_cwl_file,
+                self.app_cwl_file + "#s1-snapping-ifg",
+            ],
+        )
+        with open(self.expected_wrapped_cwl) as f:
+            contents = f.read()
+            assert result.output == contents
